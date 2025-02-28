1b1978893ec66b9548adebe08476f03224c27433
yj <cyoos1103@gmail.com>
2025-02-28 11:24:04 +0900

    [feat] Add CmDetail Component

---
+++ src/component/community/CmDetail.jsx
@@ -0,0 +1,34 @@
+const CmDetail = () => {
+  return (
+    <div className="cm-detail-container">
+      <div className="cm-detail-top">
+        <div>유저프로필{userProfile}</div>
+        <div>
+          <label>제목</label>
+        </div>
+        <div className="cm-title">{cmTitle}</div>
+      </div>
+
+      <div className="cm-detail-mid">
+        <div>
+          <label>내용</label>
+        </div>
+        <div>
+          <div>{cmDescription}</div>
+          <div>{cmIMG}</div>
+        </div>
+      </div>
+      <div className="cm-detail-bottom">
+        <div>
+          <label>작성일</label>
+        </div>
+        <div>{cmDate}</div>
+      </div>
+      <div>
+        <button>수정하기</button>
+        <button>삭제하기</button>
+      </div>
+    </div>
+  );
+};
+export default CmDetail;
