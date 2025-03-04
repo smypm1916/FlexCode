117a666fe1127f1379f80d15cfad34a855ae202d
smypm1916 <sungmin.yoon@ymail.ne.jp>
2025-03-04 12:59:32 +0900

    refactor: Restructure ProductDetail component
    - Removed data fetching logic and useState/useEffect hooks
    - Added placeholder structure for product details layout
    - Integrated Button and Select components
    - Prepared placeholders for different product detail sections

--- src/component/product/ProductDetail.jsx
+++ src/component/product/ProductDetail.jsx
@@ -1,24 +1,61 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-
+import React from "react";
+import Button from "../layout/Button";
+import Select from "../layout/Select";
 const ProductDetail = () => {
-   const { id } = useParams();
-   const [product, setProduct] = useState(null);
-
-   useEffect(() => {
-      fetch(`http://localhost:8080/api/products/${id}`)
-         .then((res) => res.json())
-         .then((data) => setProduct(data))
-         .catch((err) => console.error(err));
-   }, [id]);
-
-   if (!product) return <p>로딩 중...</p>;
 
    return (
       <div>
-         <h2>{product.name}</h2>
-         <p>{product.description}</p>
-         <p>가격: {product.price}원</p>
+         {/* 컨테이너 1 */}
+         <div>
+            {/* 이미지 */}
+            <div>img</div>
+
+            {/* 상품정보 */}
+            <div>
+               <h3>{상품타입}</h3>
+               <div>
+                  <h3>{상품명}</h3>
+                  <p>{상품설명}</p>
+                  <div>
+                     <div>
+                        <Select />
+                     </div>
+                     <div>
+                        <Select />
+                     </div>
+                  </div>
+               </div>
+
+               {/* 선택 정보 */}
+               <div>
+                  <div>
+                     {/* 장바구니 추가, 별도 컴포넌트 */}
+                  </div>
+               </div>
+
+               {/* 버튼 */}
+               <div>
+                  <Button />
+                  <Button />
+               </div>
+            </div>
+         </div>
+
+         {/* 컨테이너 2 */}
+         <div>
+            {/* 상품 이미지 컴포넌트, 스크롤 이벤트 */}
+            <img src="" alt="" />
+         </div>
+
+         {/* 컨테이너 3 */}
+         <div>
+            {/* 추가 정보 */}
+         </div>
+
+         {/* 컨테이너 4 */}
+         <div>
+            {/* 리뷰??? */}
+         </div>
       </div>
    );
 };
