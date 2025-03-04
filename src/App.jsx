cd733985bab5f808bef95db1e621121d8591a8d7
smypm1916 <sungmin.yoon@ymail.ne.jp>
2025-03-04 17:51:27 +0900

    feat: Add product detail route to App component
    - Implemented new route for individual product details
    - Added ProductDetail component to routing configuration
    - Included route parameter for product identification

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import './App.css';
 import Footer from './component/common/Footer';
 import Header from './component/common/Header';
 import Index from './component/product/Index';
+import ProductDetail from './component/product/ProductDetail';
 
 function App() {
   return (
     <Router>
       <Header />
+
+      {/* 루팅 */}
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
       </Routes>
+      {/* 루팅 */}
+
       <Footer />
     </Router>
   )
