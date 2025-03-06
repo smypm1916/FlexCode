8e57d3aebf6736203c46d0a5ec412b49b0e22dca
smypm1916 <sungmin.yoon@ymail.ne.jp>
2025-03-06 13:12:06 +0900

    chore: Update Vite configuration and environment variables
    - Change REACT_APP_IMG_PATH to VITE_IMG_PATH in .env
    - Add proxy configuration in Vite config for API requests
    - Update App.jsx routing to enable ProductDetail route and comment out unused routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
-import SignUp from "./component/account/SignUp";
 import Footer from "./component/common/Footer";
 import Header from "./component/common/Header";
 import Index from "./component/product/Index";
+import ProductDetail from "./component/product/ProductDetail";
 
 function App() {
   return (
@@ -13,10 +13,10 @@ function App() {
       {/* 루팅 */}
       <Routes>
         <Route path="/" element={<Index />} />
-        {/* <Route path="/product/:id" element={<ProductDetail />} /> */}
-        <Route path="/order" element={<Order />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        {/* <Route path="/order" element={<Order />} /> */}
         {/* <Route path="/order/:id" element={<OrderComplete />} /> */}
-        <Route path="/signup" element={<SignUp />} />
+        {/* <Route path="/signup" element={<SignUp />} /> */}
       </Routes>
       {/* 루팅 */}
 
