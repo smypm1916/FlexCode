099349d34fb65698cb8b2196fac53a8c9f60434f
smypm1916 <sungmin.yoon@ymail.ne.jp>
2025-03-06 10:11:21 +0900

    feat: Update CheckedProduct component with option and delete button

--- src/component/common/CheckedProduct.jsx
+++ src/component/common/CheckedProduct.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-
-
+import Button from './Button';
 // 제품상세, 장바구니, 주문페이지에서 사용예정
 
 const CheckedProduct = () => {
@@ -12,10 +11,19 @@ const CheckedProduct = () => {
       <div>
          <div>
             <div>
-               <img src="" alt="" />
+               {/* 상품명 */}
             </div>
             <div>
-               {/* 상품정보 */}
+               {/* 상품옵션 */}
+               <div>
+                  <div>{옵션명}</div>
+                  <div>{옵션가격}</div>
+                  <div>{구매수량}</div>
+               </div>
+               {/* 상품삭제 */}
+               <div>
+                  <Button btnTxt="X" />
+               </div>
             </div>
          </div>
       </div>
