5f4c3ccada0f120abdb7ad6e997891e2b563bd41
pchaeeun1002 <pchaeeun1002@gmail.com>
2025-03-10 11:58:28 +0900

    [modify] Add Dependencies and Product Comment

--- server/services/products.js
+++ server/services/products.js
@@ -1,35 +1,33 @@
-const db = require("../models");
-const Product_info = db.Product_info;
-const Product_option = db.Product_option;
+// const db = require("../models");
+// const Product_info = db.Product_info;
+// const Product_option = db.Product_option;
 
+// const getAllProducts = async () => {
+//    return await Product_info.findAll();
+// };
 
-const getAllProducts = async () => {
-   return await Product_info.findAll();
-};
+// const getProductDetail = async (product_no) => {
+//    return await Product_info.findByPk(product_no, {
+//       include: [
+//          {
+//             model: Product_option,
+//             attributes: ["option_no", "option_title", "option_price"],
+//          }
+//       ]
+//    });
+// };
 
-const getProductDetail = async (product_no) => {
-   return await Product_info.findByPk(product_no, {
-      include: [
-         {
-            model: Product_option,
-            attributes: ["option_no", "option_title", "option_price"],
-         }
-      ]
-   });
-};
+// const regProduct = async (data) => {
+//    try {
+//       const newProduct = await Product_info.create(data);
+//       return newProduct;
+//    }
+//    catch (error) {
+//       console.log(error);
+//       throw error;
+//    }
+// };
 
-const regProduct = async (data) => {
-   try {
-      const newProduct = await Product_info.create(data);
-      return newProduct;
-   }
-   catch (error) {
-      console.log(error);
-      throw error;
-   }
-};
+// // const getByCategory = async()
 
-// const getByCategory = async()
-
-
-module.exports = { getAllProducts, getProductDetail, regProduct };
+// module.exports = { getAllProducts, getProductDetail, regProduct };
