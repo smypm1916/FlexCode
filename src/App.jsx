9e7c825b18152c0e2c40639662260165b8d03e25
yj <cyoos1103@gmail.com>
2025-03-12 15:31:56 +0900

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,10 @@ import SignUp from "./component/account/SignUp";
 import Footer from "./component/common/Footer";
 import Header from "./component/common/Header";
 import Index from "./component/product/Index";
-import SignUp from "./component/account/SignUp";
 import ProductDetail from "./component/product/ProductDetail";
 import CmMain from "./component/community/CmMain";
-import CmAdd from "./component/community/CmAdd";
 import ProductLists from "./component/product/ProductLists";
+import CmAdd from "./component/community/CmAdd";
 
 function App() {
   return (
@@ -17,11 +16,11 @@ function App() {
       {/* 루팅 */}
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route path="/signup" element={<SignUp />} />
+        {/* <Route path="/signup" element={<SignUp />} /> */}
         {/* <Route path="/detail/:product_no" element={<ProductDetail />} /> */}
         {/* <Route path="/order" element={<Order />} /> */}
         {/* <Route path="/order/:id" element={<OrderComplete />} /> */}
-        {/* <Route path="/signup" element={<SignUp />} /> */}
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/community" element={<CmMain />} />
         <Route path="/CmAdd" element={<CmAdd />} />
         <Route path="/signup" element={<SignUp />} />
