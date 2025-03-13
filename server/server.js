561790c69ca4e9496d2329f5f07615ceb1e0ac1b
smypm1916 <sungmin.yoon@ymail.ne.jp>
2025-03-13 11:25:51 +0900

    feat: Add order routing with initial setup for order management

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const optionRouter = require("./routes/options");
 const session = require('express-session');
 const { createClient } = require('redis');
 const RedisStore = require('connect-redis').default;
+const orderRouter = require('./routes/order');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -43,6 +44,7 @@ app.use("/api/users", userRouter);
 app.use("/api/post", cmRouter);
 app.use("/api/options", optionRouter);
 app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 // 서버 실행 함수
 const startServer = async () => {
