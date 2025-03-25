6ff8a25098b0104b5d2be4f381e37410a32807ed
smypm1916 <sungmin.yoon@ymail.ne.jp>
2025-03-25 16:44:55 +0900

    feat: Add Team component to display team member names

---
+++ src/component/common/Team.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Team = () => {
+   return (
+      <>
+         <p>팀원 이름</p>
+         <p>윤성민</p>
+         <p>박채은</p>
+         <p>함유진</p>
+         <p>이민재</p>
+         {/*  */}
+         <p>
+            팀원 : 윤성민, 박채은, 함유진, 이민재
+         </p>
+      </>
+   )
+}
+
+export default Team
