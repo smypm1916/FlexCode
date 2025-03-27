ca365a4801a904a2494a55daec4b6774e9fdda32
pchaeeun1002 <pchaeeun1002@gmail.com>
2025-03-27 18:41:22 +0900

    [fix] Change Start Server

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,17 +9,17 @@ export default defineConfig({
   },
   server: {
     host: "0.0.0.0",
-    // port: 5173,
+    port: 5173,
     // strictPort: true,
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
+      "/api": {
+        target: "http://localhost:8080",
         // target: 'http://127.0.0.1:8080',
         // rewrite: (path) => path.replace(/^\/api/, ''),
         changeOrigin: true,
         secure: false,
-        ws: true
-      }
+        ws: true,
+      },
     },
   },
 });
